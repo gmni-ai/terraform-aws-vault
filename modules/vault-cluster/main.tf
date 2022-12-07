3346ebf7de2b5435be76dabbe272b5494dccf4db
Sam O <soluwalana@gmail.com>
2022-12-07 12:40:28 -0700

    Revert back to IMDSv1

--- modules/vault-cluster/main.tf
+++ modules/vault-cluster/main.tf
@@ -143,7 +143,7 @@ resource "aws_launch_template" "launch_configuration" {
   }
   metadata_options {
     http_endpoint = "enabled"
-    http_tokens   = "required"
+    http_tokens   = "optional"
   }
 }
 
